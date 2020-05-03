63e9d0203d457d9fde0e6575ef657e4840770fc0
nurlan985 <nurlan985@gmail.com>
2020-05-03 10:16:01 -0500

    Initial commit

---
+++ src/main/java/edu/miu/cs545/group01/online/market/Application.java
@@ -0,0 +1,13 @@
+package edu.miu.cs545.group01.online.market;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+
+}
